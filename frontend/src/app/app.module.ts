c0669b6d78534b2de44611926f5c34a44b6a87fc
Gyula Gombos <gombos88@gmail.com>
2022-02-17 12:09:17 +0100

    new content saved design was updated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SavedWordShowComponent } from './components/saved-word-show/saved-word-show.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SavedWordShowComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/saved-word-show/saved-word-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-word-show',
+  templateUrl: './saved-word-show.component.html',
+  styleUrls: ['./saved-word-show.component.css']
+})
+export class SavedWordShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
