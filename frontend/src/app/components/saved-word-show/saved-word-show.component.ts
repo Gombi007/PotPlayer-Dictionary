0e44ad3478beb273105aceb356b0a32a707e416d
Gyula Gombos <gombos88@gmail.com>
2022-02-18 15:39:29 +0100

    show saved items in this title

--- frontend/src/app/components/saved-word-show/saved-word-show.component.ts
+++ frontend/src/app/components/saved-word-show/saved-word-show.component.ts
@@ -14,20 +14,22 @@ export class SavedWordShowComponent implements OnInit {
   public word: string;
   public translatedWord: string;
   public date: string;
-  private id:number=0;
+  public wordsByTitle: string[];
+  private id: number = 0;
 
   constructor(
     private route: ActivatedRoute,
     private httpClient: HttpClient
   ) { }
 
-   ngOnInit():void {
-     this.route.queryParams.subscribe(value => {
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(value => {
       value.title == '' ? '' : this.title = value.title;
       value.word == '' ? '' : this.word = value.word;
-      this.id++;      
-      if(this.id === 2){
-       this.translate();
+      this.id++;
+      if (this.id === 2) {
+        this.translate();
+        this.showWordsByTitle();
       }
     });
   }
@@ -36,8 +38,17 @@ export class SavedWordShowComponent implements OnInit {
     this.httpClient.get<any>('http://localhost:8081/api/dictionary?title=' + this.title + '&word=' + this.word)
       .subscribe(
         response => {
-          this.translatedWord = response.word2;         
-          this.date = response.date;         
+          this.translatedWord = response.word2;
+          this.date = response.date;
+        });
+  }
+
+  showWordsByTitle() {
+    this.httpClient.get<any>('http://localhost:8081/api/dictionary/' + this.title)
+      .subscribe(
+        response => {
+          this.wordsByTitle = response;
+          console.log(response);
         });
   }
 
