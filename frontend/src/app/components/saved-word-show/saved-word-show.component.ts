82292018f48b395e12623b3466a688416b217bee
Gyula Gombos <gombos88@gmail.com>
2022-02-24 18:59:28 +0100

    potplayer save method was updated

--- frontend/src/app/components/saved-word-show/saved-word-show.component.ts
+++ frontend/src/app/components/saved-word-show/saved-word-show.component.ts
@@ -15,23 +15,29 @@ export class SavedWordShowComponent implements OnInit {
   title: string;
   word: string;
   wordsByTitle: string[];
-  errorResponse:string = '-';
-  actualCollectionTitle:string;
-  savedWord: Word = new Word(); 
+  errorResponse: string;
+  actualCollectionTitle: string;
+  savedWord: Word = new Word();
+  queryparamsValidateOk: boolean;
+  queryparamFailedText:string;
+  
 
   constructor(
     private route: ActivatedRoute,
     private httpClient: HttpClient
   ) { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.route.queryParamMap.subscribe(value => {
+      this.errorResponse = '-';
+      this.savedWord.word2 = undefined;
       this.title = value.get('title');
       this.word = value.get('word');
+      this.validateQueryparams();    
 
-      if (this.word !== undefined && this.title !== undefined) {
+      if (this.queryparamsValidateOk) {
         this.translate();
-        this.showWordsByTitle();        
+        this.showWordsByTitle();
       }
     });
 
@@ -40,9 +46,9 @@ export class SavedWordShowComponent implements OnInit {
   translate() {
     this.httpClient.get<any>(API.URL + '?title=' + this.title + '&word=' + this.word)
       .subscribe({
-          next: (value) => this.savedWord = value,                            
-          error: (error) => this.errorResponse = error.error.Message
-        });
+        next: (value) => this.savedWord = value,
+        error: (error) => this.errorResponse = error.error.Message
+      });
   }
 
   showWordsByTitle() {
@@ -50,9 +56,27 @@ export class SavedWordShowComponent implements OnInit {
     this.httpClient.get<any>(API.URL + '/' + this.title)
       .subscribe(
         response => {
-          this.wordsByTitle = response;          
+          this.wordsByTitle = response;
         });
-       
+
   }
+
+  validateQueryparams() {
+    this.queryparamsValidateOk = false;
+    
+    if (this.title === undefined || this.title.length <= 1 || !isNaN(Number(this.title))) {        
+      this.queryparamsValidateOk = false;     
+      this.queryparamFailedText = 'Title must be a min. 2 length text';
+      return false;     
+    }
+    if (this.word === undefined || this.word.length <= 1 || !isNaN(Number(this.word))) {
+      this.queryparamsValidateOk = false;
+      this.queryparamFailedText = 'English word must be a min. 2 length text';
+      return false;
+
+    }      
+     this.queryparamsValidateOk = true;
+     return true;
+    } 
 
 }
